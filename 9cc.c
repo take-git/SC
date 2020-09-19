1f09c3395eec0c98129bfb3b3f58416292052d8e
takerumimata <fossil1908@gmail.com>
2020-09-20 00:39:41 +0900

    update: add abstract syntax tree

--- 9cc.c
+++ 9cc.c
@@ -15,6 +15,27 @@ typedef enum {
   TK_EOF,       // 入力の終わりを表すトークン
 } TokenKind;
 
+// 抽象構文木のノードの種類
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの型
+struct Node
+{
+  NodeKind kind; // ノードの型
+  Node *lhs; // 左辺
+  Node *rhs; // 右辺
+  int val; // kindがND_NUMの場合のみ使う
+};
+
+
 typedef struct Token Token;
 
 // トークン型
