834d896dd5ac8309f0b85465d7b74463a4d2b7e3
takerumimata <fossil1908@gmail.com>
2020-09-22 22:31:02 +0900

    fix error: avoid implicit declaretion of function

--- 9cc.c
+++ 9cc.c
@@ -55,6 +55,15 @@ Token *token;
 
 // ---------------------------------------- 関数 
 
+// 次のトークンが期待している記号の時には、トークンを１つ読み進めて、真を返す。それ以外の場合には偽を返す。
+bool consume(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    return false;
+  token = token->next;
+  return true;
+}
+
+
 // エラー箇所を報告する
 void error_at(char *loc, char *fmt, ...) {
   va_list ap;
@@ -67,6 +76,26 @@ void error_at(char *loc, char *fmt, ...) {
   vfprintf(stderr, fmt, ap);
   fprintf(stderr, "\n");
   exit(1);
+}
+
+// 次のトークンが期待している記号の時には、トークンを１つ読み進める。
+// それ以外の場合にはエラーを報告する。
+void expect(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    // error("`%c'ではありません", op);
+    error_at(token->str, "数ではありません");
+  token = token->next;
+}
+
+// 次のトークンが数値の場合、トークンを１つ読み進めてその数値を返す。
+// それ以外の場合にはエラーを報告する
+int expect_number() {
+  if (token->kind != TK_NUM)
+    // error("数ではありません");
+    error_at(token->str, "数ではありません");
+  int val = token->val;
+  token = token->next;
+  return val;
 }
 
 // エラーを報告するための関数
@@ -135,34 +164,6 @@ Node *primary() {
 
   // そうでなければ数値のはず
   return new_node_num(expect_number());
-}
-
-// 次のトークンが期待している記号の時には、トークンを１つ読み進めて、真を返す。それ以外の場合には偽を返す。
-bool consume(char op) {
-  if (token->kind != TK_RESERVED || token->str[0] != op)
-    return false;
-  token = token->next;
-  return true;
-}
-
-// 次のトークンが期待している記号の時には、トークンを１つ読み進める。
-// それ以外の場合にはエラーを報告する。
-void expect(char op) {
-  if (token->kind != TK_RESERVED || token->str[0] != op)
-    // error("`%c'ではありません", op);
-    error_at(token->str, "数ではありません");
-  token = token->next;
-}
-
-// 次のトークンが数値の場合、トークンを１つ読み進めてその数値を返す。
-// それ以外の場合にはエラーを報告する
-int expect_number() {
-  if (token->kind != TK_NUM)
-    // error("数ではありません");
-    error_at(token->str, "数ではありません");
-  int val = token->val;
-  token = token->next;
-  return val;
 }
 
 bool at_eof() {
